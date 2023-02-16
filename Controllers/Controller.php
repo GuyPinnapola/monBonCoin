189d89c7210eb1f2740a5b6e92d55b8e05a7b53f
GuyPinnapola <pinnapolaguy@gmail.com>
2023-02-16 09:10:53 +0100

    controller fix

--- Controllers/Controller.php
+++ Controllers/Controller.php
@@ -21,10 +21,13 @@ class Controller
     //On appel le layout principale
     require_once ROOT . "/Views/layout.php";
   }
+
+  //Méthode pour sécuriser les saisies formulaires
+
   public static function security(){
     if(!empty($_POST)){
       foreach ($_POST as $key => $value) {
-        htmlspecialchars(trim($value));
+        $_POST[$key] = htmlspecialchars(trim($value));
       }
     }
   }
